f7356117881b744eb63cb44ed6cc2f8ad7fbb6aa
jiadhasan <jiadhasan2001@gmail.com>
2025-08-27 23:56:28 +0700

    login and registration html, css done

---
+++ login/login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles/login.css">
+</head>
+<body>
+      <div id="container">
+         <div><h1>LOGIN NOW</h1></div>
+         <div>
+            <form  action="">
+                <input  placeholder = "Enter E-mail" type="email" name="email" id="email"> <br>
+                <input  placeholder="Enter Password" type="password" name="pass" id="pass"> <br>
+                <button>LOGIN NOW</button> <br>
+                <p>dont have an account? <a id="text" href="registration.php">register now</a></p>
+            </form>
+         </div>
+      </div>
+</body>
+</html>
---
+++ login/registration.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles/registration.css">
+</head>
+<body>
+      <div id="container">
+         <div><h1>REGISTRATION NOW</h1></div>
+         <div>
+            <form  action="">
+                <input placeholder="Enter Name" type="text"><br>
+                <input  placeholder = "Enter E-mail" type="email" name="email" id="email"> <br>
+                <input  placeholder="Enter Password" type="password" name="pass" id="pass"> <br>
+                <input  placeholder="Confirm Password" type="confirm password" name="con pass" id=" con pass"> <br>
+                <input  type="file" name="" id="">
+                
+                <button>Register Now</button> <br>
+                <p>already have an account? <a id="text" href="login.php">login now</a></p>
+            </form>
+         </div>
+      </div>
+</body>
+</html>
