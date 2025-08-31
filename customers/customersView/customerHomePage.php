0dfda686a51e49760b6aae981a64da3ab7bb0f3f
jiadhasan <jiadhasan2001@gmail.com>
2025-08-31 12:40:08 +0700

    customer welcome page html,css done

--- customers/customersView/customerHomePage.php
+++ customers/customersView/customerHomePage.php
@@ -1,1 +1,38 @@
-<h1>this is customers home page</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Customres HomePage</title>
+    <link rel="stylesheet" href="styles/customerHomePage.css">
+</head>
+<body id = "bg-img">
+    <!--nav bar-->
+    <head>
+        <div id ="flex">
+                <div id = "name">Sugar & Soure</div>
+                <div >
+                    <ul>
+                      <a href="">home</a>
+                      <a href="">shop</a>
+                      <a href="">orders</a>
+                      <a href="">about</a>
+                      <a href="">contact</a>
+                     
+                    </ul>
+                </div>       
+        </div>
+     
+    </head>
+    <main>
+        <h1>WELCOME  <br> TO  OUR <br> RESTAURANT</BR></h1>
+        <P>“We don’t just serve meals; we serve experiences — crafted with love,<br> seasoned with care, and garnished with happiness.”</P>
+       
+        
+    </main>
+    <footer>
+
+    </footer>
+    
+</body>
+</html>
